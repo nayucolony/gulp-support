018fdaafc7e3aa251f336bc0f657e7fb9329b3f5
nayucolony <jpz1332@linecorp.com>
2018-05-29 01:32:42 +0900

    eslint

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,16 @@
-import "prismjs"
+import 'prismjs'
 import '../../node_modules/prismjs/themes/prism.css'
 import '../../node_modules/prismjs/components/prism-css'
 import '../../node_modules/prismjs/components/prism-scss'
 import '../../node_modules/prismjs/components/prism-javascript'
 import '../../node_modules/prismjs/components/prism-bash'
 import '../../node_modules/prismjs/components/prism-json'
-import "prismjs/plugins/toolbar/prism-toolbar.css";
-import "prismjs/plugins/toolbar/prism-toolbar.min.js";
+import 'prismjs/plugins/toolbar/prism-toolbar.css'
+import 'prismjs/plugins/toolbar/prism-toolbar.min.js'
 import '../../node_modules/prismjs/components/prism-markup-templating'
 import '../../node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js'
 import '../../node_modules/prismjs/plugins/line-numbers/prism-line-numbers.css'
 import '../../node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
-
-
 
 const message = `
 
